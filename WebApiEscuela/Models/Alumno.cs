374f6b4b4970fa38eab40363049a9ccb70130e4c
DeniConiglio <deniseconiglio2017@outlook.com>
2022-11-20 16:58:24 -0300

    Agregar archivos de proyecto.

---
+++ WebApiEscuela/Models/Alumno.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace WebApiEscuela.Models
+{
+    public class Alumno
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Matricula { get; set; }
+
+        
+    }
+}
+
